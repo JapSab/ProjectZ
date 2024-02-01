58518aff6ff905bddddeb65cc8d3e118466da590
tatopk <t.rekhviashvili@mima-poker.com>
2024-02-01 22:22:15 +0400

    finished dockerizing and database connection

--- API/__init__.py
+++ API/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
 from pymongo import MongoClient
-from dotenv import load_dotenv
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flasgger import Swagger
 import os
 
-load_dotenv()
-
 app = Flask(__name__)
+swagger = Swagger(app)
 CORS(app)
 
-mongo_uri = os.getenv("MONGO_URI")
-db_name = os.getenv("DATABASE_NAME")
-secret_key = os.getenv("SECRET_KEY")
-jwt_secret_key = os.getenv("JWT_KEY")
+mongo_uri = os.environ.get("MONGO_URI")
+db_name = os.environ.get("DATABASE_NAME")
+secret_key = os.environ.get("SECRET_KEY")
+jwt_secret_key = os.environ.get("JWT_KEY")
 app.config['JWT_SECRET_KEY'] = jwt_secret_key
 client = MongoClient(mongo_uri)
 db = client[db_name]
--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from API import app
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
-
 @app.route('/dashboard')
 @jwt_required()
 def hello():
