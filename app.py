756f2d043dd2fec92599e56d74b9eb85241670d9
JapSab <sabaj127001@gmail.com>
2024-07-09 14:41:06 +0400

    ready for tatos check

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from API import app
 from API.utils.tokenizer import admin_required
 
-@app.route('/dashboard')
-@admin_required
-def hello():
-    return 'Hello'
+# @app.route('/dashboard')
+# @admin_required
+# def hello():
+#     return 'Hello'
 
-@app.route('/')
-def homepage():
-    return 'it work'
+# @app.route('/')
+# def homepage():
+#     return 'it work'
 
 if __name__ == '__main__':
     app.run(debug=True)
